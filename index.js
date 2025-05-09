cce77166b4d02f7609c57bc2d93f27761efbc054
Maria Mactzul <mariamactzul200@gmail.com>
2025-05-09 13:05:28 -0600

    ToDo

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ let botton = document.querySelector('#button-addon2')
 let div1 = document.querySelector('#listaTareas')
 let input1=document.querySelector('.form-control')
 
-
-const List = (event) => {
-    event.preventDefault()
-
+const List = () => {
+  
     let div = document.createElement('div')
     let input =  document.createElement('input')
     let button1 =  document.createElement('button')
     let button2 =  document.createElement('button')
     div.classList.add("input-group", "mb-3")
     input.classList.add("form-control")
-    button1.classList.add("btn", "btn-outline-primary")
+    button1.classList.add("btn", "btn-outline-success")
     button2.classList.add("btn", "btn-outline-danger")
    
     button1.innerText="Eliminar"
@@ -38,5 +36,31 @@ const List = (event) => {
 }
 
 
-botton.addEventListener('click',List)
+input1.addEventListener('keydown',(event)=>{
+    if(event.key=="Enter"){
+    
+        if(String(input1.value).trim()=="")
+            { 
+                input1.value=''
+                input1.placeholder="No se agragaron TAREAS"
+            }
+            else{ 
+                input1.placeholder='Agregue una tarea'
+                List()
+            }
+    }
 
+})
+
+
+botton.addEventListener('click', () => {
+    if(String(input1.value).trim()=="")
+    { 
+        input1.value=''
+        input1.placeholder="No se agragaron TAREAS"
+    }
+    else{ 
+        input1.placeholder='Agregue una tarea'
+        List()
+    }
+})
